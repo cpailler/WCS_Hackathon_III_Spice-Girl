e14c55e7dda6ef3bd95ed279004f2865e60d7b2d
Jennifer Lieby <ze_nifer@hotmail.com>
2018-07-12 13:19:00 +0200

    debut page d accueil

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,12 @@ var Encore = require('@symfony/webpack-encore');
 Encore
     .setOutputPath('web/build/')
     .setPublicPath('/web')
+    .setManifestKeyPrefix('build/')
+
     .addEntry('app', './assets/js/app.js')
     .addEntry('style', './assets/scss/main.scss')
+    .addEntry('homepage', './assets/scss/homepage.scss')
+    .addEntry('homepageJS', './assets/js/homepageJS.js')
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSassLoader();
