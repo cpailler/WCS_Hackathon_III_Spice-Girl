35294aaf633c2d6c35f9852e823249970efaeb05
Tiffanym <tiffany.maurath@orange.fr>
2018-07-12 13:07:55 +0200

    Ajout game.html.twig et son CSS/JS

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,10 @@ Encore
     .setPublicPath('/web')
     .addEntry('app', './assets/js/app.js')
     .addEntry('style', './assets/scss/main.scss')
+
+    .addEntry('css/stylegame', './assets/scss/stylegame.scss')
+    .addEntry('js/stylegame', './assets/js/stylegame.js')
+
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSassLoader();
