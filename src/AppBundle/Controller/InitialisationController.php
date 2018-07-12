5501a0339992257422eaa5fe877bbe21fb096a40
ccoralie <coraliecotte.cc@gmail.com>
2018-07-12 14:26:29 +0200

    ajout méthode initialisation

--- src/AppBundle/Controller/InitialisationController.php
+++ src/AppBundle/Controller/InitialisationController.php
@@ -2,8 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Atout;
+use AppBundle\Entity\Banque;
 use AppBundle\Entity\Departement;
 use AppBundle\Entity\Developpement;
+use AppBundle\Entity\FacteurInflationGlobal;
+use AppBundle\Entity\FacteurPollutionGlobal;
+use AppBundle\Entity\NiveauPollutionGlobal;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -16,37 +21,68 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class InitialisationController extends Controller
 {
     /**
-     * @Route("/", name="initalise_departement")
+     * @Route("/", name="initial")
      */
-    public function initialiseDepartementAction()
+    public function initialisationAction()
     {
         $em = $this->getDoctrine()->getManager();
         $departements = $em->getRepository(Departement::class)->findAll();
+        $developpements = $em->getRepository(Developpement::class)->findAll();
+        $facteurInflationGlobals = $em->getRepository(FacteurInflationGlobal::class)->findAll();
+        $facteurPollutionGlobals = $em->getRepository(FacteurPollutionGlobal::class)->findAll();
+        $niveauPollutionGlobals = $em->getRepository(NiveauPollutionGlobal::class)->findAll();
+        $atouts = $em->getRepository(Atout::class)->findAll();
+        $banques = $em->getRepository(Banque::class)->findAll();
 
         foreach ($departements as $departement) {
             $departement->setUsine(0);
-            $departement->setNiveauPollution(rand(0,5));
+            $niveauxPollution[] = $departement->setNiveauPollution(rand(0,5))->getNiveauPollution();
             $em->persist($departement);
-            $em->flush();
+            $em->persist($departement[floor(rand(0,10))]->setUsine(1));
 
         }
-        $this->redirectToRoute('page_jeu');
-
-
-    }
-
-    public function initialiseDeveloppementAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-        $developpements = $em->getRepository(Developpement::class)->findAll();
 
         foreach ($developpements as $developpement) {
             $developpement->setEffectif(0);
             $em->persist($developpement);
-            $em->flush();
 
         }
+
+        foreach ($facteurInflationGlobals as $facteurInflationGlobal) {
+            $facteurInflationGlobal->setFacteur(1);
+            $em->persist($facteurInflationGlobal);
+
+        }
+
+        foreach ($facteurPollutionGlobals as $facteurPollutionGlobal) {
+            $facteurPollutionGlobal->setFacteur(1);
+            $em->persist($facteurPollutionGlobal);
+
+        }
+        foreach ($niveauPollutionGlobals as $niveauPollutionGlobal) {
+            //calcul en fonction du niveau initial des regions
+
+            $sommeNiveauPollutionGlobal = array_sum($niveauxPollution)/10;
+            $niveauPollutionGlobal->setNiveauGlobal($sommeNiveauPollutionGlobal);
+            $em->persist($niveauPollutionGlobal);
+
+        }
+        foreach ($atouts as $atout) {
+            $atout->setQuantite(0);
+            $em->persist($atout);
+
+        }
+
+        foreach ($banques as $banque) {
+            $banque->setMoney(500);
+            $em->persist($banque);
+
+        }
+
+        $em->flush();
         $this->redirectToRoute('page_jeu');
+
+
     }
 
 
